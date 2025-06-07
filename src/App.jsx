3f12454df550d72d5409fc2f1685a5e4578e2f8b
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-06-07 15:51:38 +0100

    Fix rerouting error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage/LandingPage.jsx'
 import ArticlePage from './components/Article/ArticlePage.jsx'
 import BlogPage from './components/BlogPage/BlogPage.jsx'
