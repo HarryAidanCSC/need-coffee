fc89312519d9c2e2c4ccac5dbeaababc6bf974bb
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-06-07 15:23:32 +0100

    Feat: Deployment specs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/need-coffee/", // Replace 'need-coffee' with your actual GitHub repository name
 });
