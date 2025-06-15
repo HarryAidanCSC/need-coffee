158ad59e3d32b2a54aa71ce956b8aeec913dab17
ChuckleScriptCraft <hxzzmxnch@gmail.com>
2025-06-15 14:18:00 +0100

    Housekeeping: Clean config yml

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/need-coffee/", // Replace 'need-coffee' with your actual GitHub repository name
+  base: "/need-coffee/",
 });
